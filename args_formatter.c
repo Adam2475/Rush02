e82baf9a865cabe78875062133d7c2d340257faf
Adam Natan Passaretti <adapassa@c2r3p2.42firenze.it>
2023-12-02 18:29:06 +0100

    scheletro formatter e find

--- args_formatter.c
+++ args_formatter.c
@@ -6,13 +6,27 @@
 /*   By: adapassa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:35:25 by adapassa          #+#    #+#             */
-/*   Updated: 2023/12/02 14:37:59 by adapassa         ###   ########.fr       */
+/*   Updated: 2023/12/02 16:30:36 by adapassa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	*format_args(char *str)
+#include "lib_rush.h"
+
+int	*format_args(char *str, int size)
 {
 	int	i;
-	int	tmp;
-	tmp
+	int	j;
+	int	*tmp;
+
+	i = 0;
+	j = 0;
+	
+	while (str[i] != '\0')
+	{
+		i++;
+		j++;
+	}
+	return (tmp);
 }
+
+//conversione
