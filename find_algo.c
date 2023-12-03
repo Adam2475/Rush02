4d360dbcf34e596a8b49d6f480702e3c016e0b66
Adam Natan Passaretti <adapassa@c1r3p3.42firenze.it>
2023-12-03 12:04:48 +0100

    impostati i cases

--- find_algo.c
+++ find_algo.c
@@ -5,25 +5,40 @@ int	find_digits(char *nbr)
 	return (ft_strlen(nbr));
 }
 
-void	find_cases(t_dict *dict, char *nb)
+int	select_magnitude(char *nb)
+{
+	int caso;
+	else if (digits == 3)
+		return (caso = 3);
+	else if (digits == 4)
+		return (caso = 4);
+	else if (digits == 5)
+		return (caso = 5);
+	else if (digits == 6)
+		return (caso = 6);
+	else if (digits == 7)
+		return (caso = 7);
+	else if (digits == 8)
+		return (caso = 8);
+	else if (digits == 9)
+		return (caso = 9);
+	else if (digits == 10)
+		return (caso = 10);
+}
+
+void	find_cases(t_dict *dict, char *nb, int num)
 {
 	int	size;
 	int	magnitude;
 	size = find_digits(nb);
-	magnitude = select_case(nb);
+	
+	if (num < 20)
+		execute_case_teen(dict, nb, num);
+	else
+	{
+		magnitude = select_case(nb);
+		execute_case_full(nb, dict, num, magnitude);
+	}
 }
 
-int	select_case()
-{
-	if (digits == 1)
-	else if (digits == 2)
-	return 2
-	else if (digits == 3)
-	else if (digits == 4)
-	else if (digits == 5)
-	else if (digits == 6)
-	else if (digits == 7)
-	else if (digits == 8)
-	else if (digits == 9)
-	else if (digits == 10)
-}
+
